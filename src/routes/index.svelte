ea0624a38af8cab96d67e464923a7d91f55934e0
desmondchiam18 <desmondchiam18@gmail.com>
2022-02-13 04:15:26 +0000

    lianze dashboard v2

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -11,10 +11,187 @@
 
    	 if (error) alert(error.message); // alert if error
     }
+    let timetable = {
+	Monday: [
+  	{
+    	name: "PH",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "PM",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "BI",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "R",
+    	period: 1,
+    	style: "table-success",
+  	},
+  	{
+    	name: "BM",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "M3",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "BC",
+    	period: 2,
+    	style: "",
+  	},
+	],
+	Tuesday: [
+  	{
+    	name: "PJPK",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "M3",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "BI",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "E",
+    	period: 1,
+    	style: "table-success",
+  	},
+  	{
+    	name: "BM",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "BC",
+    	period: 3,
+    	style: "",
+  	},
+  	{
+    	name: "PJPK",
+    	period: 1,
+    	style: "",
+  	},
+	],
+	Wednesday: [
+  	{
+    	name: "BC",
+    	period: 3,
+    	style: "",
+  	},
+  	{
+    	name: "PM",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "H",
+    	period: 1,
+    	style: "table-success",
+  	},
+  	{
+    	name: "PKS",
+    	period: 3,
+    	style: "",
+  	},
+  	{
+    	name: "BM",
+    	period: 2,
+    	style: "",
+  	},
+	],
+	Thursday: [
+  	{
+    	name: "SA",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "PJPK",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "BM",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "A",
+    	period: 1,
+    	style: "table-success",
+  	},
+  	{
+    	name: "PM",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "BC",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "M3",
+    	period: 2,
+    	style: "",
+  	},
+	],
+	Friday: [
+  	{
+    	name: "BI",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "BM",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "T",
+    	period: 1,
+    	style: "table-success",
+  	},
+  	{
+    	name: "BC",
+    	period: 2,
+    	style: "",
+  	},
+  	{
+    	name: "PM",
+    	period: 1,
+    	style: "",
+  	},
+  	{
+    	name: "SA",
+    	period: 2,
+    	style: "",
+  	},
+	],
+  };
 
 </script>
 
-<div>
+
+
+
+
+
+<div class="container">
     <h1>My Dashboard</h1>
  <table class="table text-center table-striped table caption-top">
     <caption>School Timetable</caption>
@@ -37,23 +214,58 @@
     <tbody>
       <tr>
         <th scope="row" class="table-dark">MON</th>
-
+        {#each timetable.Monday as timeSlot}
+          <td colspan= {timeSlot.period} class= {timeSlot.style}>
+            <button class="btn" type="button">
+                {timeSlot.name}
+             </button>
+          </td>
+          
+        {/each}
       </tr>
       <tr>
         <th scope="row" class="table-dark">TUE</th>
-        
+        {#each timetable.Tuesday as timeSlot}
+          <td colspan= {timeSlot.period} class= {timeSlot.style}>
+            <button class="btn" type="button">
+                {timeSlot.name}
+             </button>
+          </td>
+          
+        {/each}
       </tr>
       <tr>
         <th scope="row" class="table-dark">WED</th>
-        
+        {#each timetable.Wednesday as timeSlot}
+          <td colspan= {timeSlot.period} class= {timeSlot.style}>
+            <button class="btn" type="button">
+                {timeSlot.name}
+             </button>
+          </td>
+          
+        {/each}
       </tr>
       <tr>
         <th scope="row" class="table-dark">THU</th>
-        
+        {#each timetable.Thursday as timeSlot}
+          <td colspan= {timeSlot.period} class= {timeSlot.style}>
+            <button class="btn" type="button">
+                {timeSlot.name}
+             </button>
+          </td>
+         
+        {/each}
       </tr>
       <tr>
         <th scope="row" class="table-dark">FRI</th>
-        
+        {#each timetable.Friday as timeSlot}
+          <td colspan= {timeSlot.period} class= {timeSlot.style}>
+            <button class="btn" type="button">
+                {timeSlot.name}
+             </button>
+          </td>
+         
+        {/each}
       </tr>
     </tbody>
  </table>
